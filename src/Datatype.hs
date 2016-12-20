6174ed61c0fd17c5e1c1d661ace1ec86583028e7
iyahoo <s1200191@gmail.com>
2016-12-20 17:56:22 +0900

    [Add] Datatype LispVal

---
+++ src/Datatype.hs
@@ -0,0 +1,8 @@
+module Datatype where
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
