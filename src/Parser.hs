d6ebb4f1cb843aaa03bb33f00e8c9bd4029e7b23
iyahoo <s1200191@gmail.com>
2017-01-28 00:51:43 +0900

    [Fix] 文章を修正

--- src/Parser.hs
+++ src/Parser.hs
@@ -144,4 +144,9 @@ printString :: Either ParseError LispVal -> IO ()
 printString (Right (String s)) = putStrLn s
 printString _ = putStrLn "Print error"
 
-
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
